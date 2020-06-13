6cd724fbc46b230c062bc9ee057058b4a93641de
nanako kan <n.k.0213.0529@gmail.com>
2020-06-13 11:20:40 +0900

    手順投稿機能の作成

--- app/controllers/surgical_operations_controller.rb
+++ app/controllers/surgical_operations_controller.rb
@@ -4,6 +4,16 @@ class SurgicalOperationsController < ApplicationController
   end
 
   def new
-    
+    @surgical_operation = SurgicalOperation.new
+  end
+
+  def create
+    SurgicalOperation.create(surgical_operation_params)
+    redirect_to new_surgical_operation_path
+  end
+
+  private
+  def surgical_operation_params
+    params.require(:surgical_operation).permit(:title, :content)
   end
 end
