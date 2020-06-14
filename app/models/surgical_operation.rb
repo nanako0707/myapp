d02c90a0cd53c0657782313c103012e836936cb5
nanako kan <n.k.0213.0529@gmail.com>
2020-06-14 21:05:27 +0900

    手順のストック機能実装

--- app/models/surgical_operation.rb
+++ app/models/surgical_operation.rb
@@ -1,7 +1,6 @@
 class SurgicalOperation < ApplicationRecord
   belongs_to :user
   has_many :stocks, dependent: :destroy
-  has_many :stock_users, through: :stocks, source: :user
   validates :title, presence: true
   validates :content, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :surgical_operations
+  has_many :surgical_operations, dependent: :destroy
   has_many :stocks, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
