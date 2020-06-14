2d7d848ef1012f111881e4f9e186e7236a45889f
nanako kan <n.k.0213.0529@gmail.com>
2020-06-14 10:27:49 +0900

    deviseを使用した管理画面の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'surgical_operations#index'
   resources :surgical_operations
+  devise_for :users
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
