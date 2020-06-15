10e02614c282ae698316636b6d004242f32127a8
nanako kan <n.k.0213.0529@gmail.com>
2020-06-15 19:49:13 +0900

    タイムゾーンを日本(東京)に設定

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,10 @@
 require_relative 'boot'
-
 require 'rails/all'
-
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
 module Myapp
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.i18n.default_locale = :ja
