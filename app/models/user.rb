41fc30e57e6f49df14311d23a6f0240a9cfbff52
kan nanako <n.k.0213.0529@gmail.com>
2020-07-12 19:32:35 +0900

    管理ユーザー削除を制御する設定をモデルに追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   validates :name, uniqueness: true, length: { minimum: 4, maximum: 20 }
+  before_destroy :do_not_destroy_last_admin
+
+  private
+  def do_not_destroy_last_admin
+    throw(:abort) if User.where(admin: true).count <= 1 && self.admin?
+  end
 end
