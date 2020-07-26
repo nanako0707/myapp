f260cb3289e2cad4f2b31528f98409887f509d16
kan nanako <n.k.0213.0529@gmail.com>
2020-07-26 22:45:48 +0900

    メール送信機能のテスト実施

--- spec/mailers/surgical_operation_mailer_spec.rb
+++ spec/mailers/surgical_operation_mailer_spec.rb
@@ -2,18 +2,21 @@ require 'rails_helper'
 RSpec.describe SurgicalOperationMailer, type: :mailer, js: true do
   before do
     @user = create(:user)
+    @surgical_operation = create(:surgical_operation, user: @user)
+    @users = User.all
   end
-  let(:surgical_operation){ SurgicalOperation.create(id: 1, title: 'test', content: 'test-content', user: @user) }
-  
-    it 'メールは正しく送信されているか' do
-      users = User.all
-      SurgicalOperationMailer.surgical_operation_mail(users, surgical_operation).deliver_now
-      mail = ActionMailer::Base.deliveries.last
-      body = mail.body.parts.find {|p| p.content_type.match /html/}.body.raw_source
-      it { expect(mail.from).to eq('from@example.com') }
-      it { expect(mail.to).to eq(users.map{ |u| u.email }) }
-      it { expect(mail.subject).to eq("手順作成・編集時の確認メール") }
-      it { expect(body).to match(/title: test/) }
-      it { expect(body).to match(/content: test-content/) }
-    end 
+
+  describe 'メール送信' do
+    subject(:mail) do
+      SurgicalOperationMailer.surgical_operation_mail(@users, @surgical_operation).deliver_now
+      ActionMailer::Base.deliveries.last
+    end
+
+    it 'メールの内容は正しいか' do
+      expect(mail.from).to eq(['from@example.com'])
+      expect(mail.to).to eq(@users.map{ |u| u.email }) 
+      expect(mail.subject).to eq("手順作成・編集時の確認メール")
+      expect(mail.body).to match(/title/)
+    end
+  end
 end
