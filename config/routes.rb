01783874f142a4e0e8df7404b1e9061d58ee5898
kan nanako <n.k.0213.0529@gmail.com>
2020-07-27 17:42:43 +0900

    商品の購入機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,12 @@ Rails.application.routes.draw do
       post 'delete', to: 'cards#delete'
     end
   end
+
+  resources :purchases, only: [:index] do
+    collection do
+      get 'index', to: 'purchases#index'
+      post 'pay', to: 'purchases#pay'
+      get 'done', to: 'purchases#done'
+    end
+  end
 end
