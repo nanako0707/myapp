58fba541e39a48aab4e574a273cb438d80993536
kan nanako <n.k.0213.0529@gmail.com>
2020-08-11 15:49:31 +0900

    通知画面の修正

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -4,10 +4,9 @@ module NotificationsHelper
     @comment = nil
     your_surgical_operation = link_to 'あなたの投稿', surgical_operation_path(notification), style:"font-weight: bold;"
     @visitor_comment = notification.comment_id
-    #notification.actionがfollowかlikeかcommentか
     case notification.action
       when "stock" then
-        tag.a(notification.visitor.name, style:"font-weight: bold;")+"が"+tag.a('あなたの投稿', href:surgical_operation_path(notification.surgical_operation_id), style:"font-weight: bold;")+"をストックしました"
+        tag.a(notification.visitor.name, style:"font-weight: bold;")+"さんが"+tag.a('あなたの投稿', href:surgical_operation_path(notification.surgical_operation_id), style:"font-weight: bold;")+"をストックしました"
       when "comment" then
           @comment = Comment.find_by(id: @visitor_comment)&.content
           tag.a(@visitor.name, style:"font-weight: bold;")+"さんが"+tag.a('あなたの投稿', href:surgical_operation_path(notification.surgical_operation_id), style:"font-weight: bold;")+"にコメントしました"
