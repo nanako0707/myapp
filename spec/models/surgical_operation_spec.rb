b3c385df15f661f00429ea7ca79350bdf65352c4
kan nanako <n.k.0213.0529@gmail.com>
2020-08-12 16:16:16 +0900

    バリデーションテスト実施

--- spec/models/surgical_operation_spec.rb
+++ spec/models/surgical_operation_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe SurgicalOperation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe '手順管理機能', type: :model do
+  it 'titleが空ならバリデーションが通らない' do
+    surgical_operation = SurgicalOperation.new(title: '', content: '失敗テスト', medical_department: '失敗テスト')
+    expect(surgical_operation).not_to be_valid
+  end
+  it 'contentが空ならバリデーションが通らない' do
+    surgical_operation = FactoryBot.build(:surgical_operation, title: '失敗テスト', content: '', medical_department: '失敗テスト')
+    expect(surgical_operation).not_to be_valid
+  end
+  it 'medical_departmentが空ならバリデーションが通らない' do
+    surgical_operation = FactoryBot.build(:surgical_operation, title: '失敗テスト', content: '失敗テスト', medical_department: '')
+    expect(surgical_operation).not_to be_valid
+  end
+
+  it 'titleとcontentとmedical_departmentに内容が記載されていればバリデーションが通る' do
+    surgical_operation = FactoryBot.build(:new_surgical_operation, title: '', content: '', medical_department: '')
+    expect(surgical_operation).not_to be_valid
+    expect(surgical_operation.errors[:title]).to be_present
+    expect(surgical_operation.errors[:content]).to be_present
+    expect(surgical_operation.errors[:medical_department]).to be_present
+  end
 end
