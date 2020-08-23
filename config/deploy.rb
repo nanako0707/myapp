c0efeeeb5d6bcbc81d1265082cf9c8ac6737556c
kan nanako <n.k.0213.0529@gmail.com>
2020-08-23 17:45:54 +0900

    rbenv not installエラー解消

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :deploy_to, '/var/www/myapp'
 set :repo_url,  'https://github.com/nanakobaby/myapp'
 set :linked_files, fetch(:linked_files, []).push("config/master.key")
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-set :rbenv_type, :user
+set :rbenv_type, :system
 set :rbenv_ruby, '2.6.5'
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/myapp-key.pem'] 
