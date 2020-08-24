2ca68ae8ae6432e5d42f8d5783cd8bc866730f21
kan nanako <n.k.0213.0529@gmail.com>
2020-08-24 16:10:12 +0900

    change: unicorn.conf.rb

--- config/unicorn.conf.rb
+++ config/unicorn.conf.rb
@@ -1,6 +1,6 @@
 $worker  = 2
   $timeout = 30
-  $app_dir = "/Users/kannanako/OriginalApplication/myapp"
+  $app_dir = "/myapp"
   $listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
   $pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
   $std_log = File.expand_path 'log/unicorn.log', $app_dir
