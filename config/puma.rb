65ca7d0444e07709f116ec5e4b4b8577f95fb3e0
kan nanako <n.k.0213.0529@gmail.com>
2020-08-28 09:44:27 +0900

    puma nginx設定 修正

--- config/puma.rb
+++ config/puma.rb
@@ -1,7 +1,7 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
-port        ENV.fetch("PORT") { 3000 }
-environment ENV.fetch("RAILS_ENV") { "development" }
+# port        ENV.fetch("PORT") { 3000 }
+environment ENV.fetch("RAILS_ENV") { "production" }
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 plugin :tmp_restart
 app_root = File.expand_path("../..", __FILE__)
