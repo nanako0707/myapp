68710f41e5ffb9c013fc2e688541fa86bc4b3753
kan nanako <n.k.0213.0529@gmail.com>
2020-08-29 17:09:00 +0900

    SendGrid API メール送信機能実装

--- lib/mail/send_grid.rb
+++ lib/mail/send_grid.rb
@@ -1,17 +1,35 @@
-class Mail::SendGrid
-  def initialize(settings)
-    @settings = settings
-  end
+module Mail
+  class SendGrid
+    def initialize(settings)
+      @settings = settings
+    end
 
-  def deliver!(mail)
-    from = SendGrid::Email.new(email: mail.from.first)
-    to = SendGrid::Email.new(email: mail.to.first)
-    subject = mail.subject
-    content = SendGrid::Content.new(type: 'text/plain', value: mail.body.raw_source)
-    sg_mail = SendGrid::Mail.new(from, subject, to, content)
+    def deliver!(mail)
+      ## to, cc, bcc はpersonalizationで設定する
+      personalization = ::SendGrid::Personalization.new
+      personalization.add_to(::SendGrid::Email.new(email: mail.to.first))
+      personalization.subject = mail.subject
 
-    sg = SendGrid::API.new(api_key: @settings[:api_key])
-    response = sg.client.mail._('send').post(request_body: sg_mail.to_json)
-    puts response.status_code
+      Array(mail.bcc).each do |email|
+        personalization.add_bcc(::SendGrid::Email.new(email: email))
+      end
+      Array(mail.cc).each do |email|
+        personalization.add_cc(::SendGrid::Email.new(email: email))
+      end
+
+      ## from, subject, contentを設定
+      sg_mail = ::SendGrid::Mail.new
+      sg_mail.from = ::SendGrid::Email.new(email: mail.from.first)
+      sg_mail.subject = mail.subject
+      sg_mail.add_content(::SendGrid::Content.new(type: 'text/html', value: mail.body.raw_source))
+
+      ## 送信するためのメールオブジェクト完成
+      sg_mail.add_personalization(personalization)
+
+      # API KEY 
+      sg = ::SendGrid::API.new(api_key: @settings[:api_key])
+      response = sg.client.mail._('send').post(request_body: sg_mail.to_json)
+      response.status_code
+    end
   end
 end
