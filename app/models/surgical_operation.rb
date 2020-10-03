981c1e8ba70fa2fad67cb57f41269cb1356b6bf8
nanako kan <n.k.0213.0529@gmail.com>
2020-10-03 15:41:25 +0900

    surgical_operation.rb修正(#91)

--- app/models/surgical_operation.rb
+++ app/models/surgical_operation.rb
@@ -24,17 +24,17 @@ class SurgicalOperation < ApplicationRecord
     notification.save if notification.valid?
   end
 
-  def create_notification_comment!(current_user, comment_id)
+  def create_notification_comment(current_user, comment_id)
     # 自分以外にコメントしている人をすべて取得し、全員に通知を送る
     temp_ids = Comment.select(:user_id).where(surgical_operation_id: id).where.not(user_id: current_user.id).distinct
     temp_ids.each do |temp_id|
-      save_notification_comment!(current_user, comment_id, temp_id['user_id'])
+      save_notification_comment(current_user, comment_id, temp_id['user_id'])
     end
     # まだ誰もコメントしていない場合は、投稿者に通知を送る
-    save_notification_comment!(current_user, comment_id, user_id) if temp_ids.blank?
+    save_notification_comment(current_user, comment_id, user_id) if temp_ids.blank?
   end
 
-  def save_notification_comment!(current_user, comment_id, visited_id)
+  def save_notification_comment(current_user, comment_id, visited_id)
     # コメントは複数回することが考えられるため、１つの投稿に複数回通知する
     notification = current_user.active_notifications.new(
       surgical_operation_id: id,
